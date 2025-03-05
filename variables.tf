af29e761517825d43e2cd2bc5ef24052636994ca
marufaus <maruf.aus@hotmail.com>
2025-03-05 15:24:48 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,14 @@ variable "stage" {
   type        = string
 }
 
+variable "s3_bucket" {
+  description = "The S3 bucket name"
+  type        = string
+}
+
+variable "s3_bucket_key" {
+  description = "The S3 bucket key name"
+  type        = string
+}
+
+
