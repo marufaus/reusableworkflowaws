d4c159a8d0c98efd41653232b68be1a2f6b4e36e
marufaus <maruf.aus@hotmail.com>
2025-03-06 16:49:28 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,11 +2,12 @@ terraform {
   backend "s3" {}
 }
 
+/*
 assume_role {
     role_arn     = "arn:aws:iam::992092700795:role/Dev_acc_s3_permission"
     session_name = "TerraformSession"
   }
-}
+} */
 
 
 provider "aws" {
